971fbaa7fe19ae973aae5a26bacbc0611f69c33f
Flavius Dolha <flaviusdolha@icloud.com>
2020-12-16 22:50:44 +0200

    Create Comment.cs

---
+++ Model/Domain/Comment.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Chelsea.Model.Domain
+{
+    public class Comment
+    {
+        private readonly int _id;
+        public int Id
+        {
+            get => _id;
+        }
+        
+        private readonly int _authorId;
+        public int AuthorId
+        {
+            get => _authorId;
+        }
+
+        private readonly DateTime _creationDate;
+        public DateTime CreationDate
+        {
+            get => _creationDate;
+        }
+
+        private string _content;
+        public string Content
+        {
+            get => _content;
+            set => _content = value;
+        }
+    }
+}
