e67dd81f138cdd05eeb1b3c45d50598211bf2035
Flavius Dolha <flaviusdolha@icloud.com>
2020-12-17 13:15:53 +0200

    Create User and Roles

---
+++ Model/Domain/User/Role.cs
@@ -0,0 +1,9 @@
+namespace Chelsea.Model.Domain.User
+{
+    public enum Role : ushort
+    {
+        Viewer = 0,
+        Manager = 1,
+        Admin = 2
+    }
+}
---
+++ Model/Domain/User/User.cs
@@ -0,0 +1,71 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chelsea.Model.Domain.User
+{
+    public class User
+    {
+        /*
+        * ==========================================
+        * PROPERTIES
+        * ==========================================
+        */
+        
+        private int _id;
+        public int Id
+        {
+            get => _id;
+        }
+        
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        [DataType(DataType.EmailAddress)]
+        private string _email;
+        [DataType(DataType.EmailAddress)]
+        public string Email
+        {
+            get => _email;
+        }
+
+        private string _profilePictureUrl;
+        public string ProfilePictureUrl
+        {
+            get => _profilePictureUrl;
+            set => _profilePictureUrl = value;
+        }
+
+        private Role _role;
+        public Role Role
+        {
+            get => _role;
+            set => _role = value;
+        }
+
+        /*
+        * ==========================================
+        * CONSTRUCTORS
+        * ==========================================
+        */
+        
+        public User(int id, string firstName, string lastName, string email)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _profilePictureUrl = "";
+            _role = Role.Viewer;
+        }
+    }
+}
