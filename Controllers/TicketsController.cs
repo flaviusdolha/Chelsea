83a7025ec98edf11242a6e126e3dbc7cb3defe9e
Flavius Dolha <flaviusdolha@icloud.com>
2020-12-27 12:00:04 +0200

    Update TicketsController.cs

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Chelsea.Model.Domain.Ticket;
 using Chelsea.Model.Service;
