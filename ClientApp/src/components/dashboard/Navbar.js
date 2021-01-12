d4c12d51827732bafe9e3181710d4204d132491e
Flavius Dolha <flaviusdolha@icloud.com>
2021-01-12 11:19:47 +0200

    Fix className NavBar

--- ClientApp/src/components/dashboard/Navbar.js
+++ ClientApp/src/components/dashboard/Navbar.js
@@ -8,12 +8,12 @@ function Navbar() {
             </div>
             <div className="flex flex-wrap content-center group">
                 <div className="flex flex-wrap content-center">
-                    <i class="fas fa-caret-down text-sm fill text-white group-hover:text-gray-300"></i>
+                    <i className="fas fa-caret-down text-sm fill text-white group-hover:text-gray-300"></i>
                 </div>
                 <div className="flex flex-wrap content-center ml-2">
                     <p className="text-white text-sm group-hover:text-gray-300">Flavius Dolha</p>
                 </div>
-                <i class="fas fa-user-circle ml-4 text-2xl fill text-white group-hover:text-gray-300"></i>
+                <i className="fas fa-user-circle ml-4 text-2xl fill text-white group-hover:text-gray-300"></i>
             </div>
         </div>
     );
