24c4bdf94835d827aa2f3ec5291b93d4c193836b
Flavius Dolha <flaviusdolha@icloud.com>
2021-01-13 17:27:58 +0200

    Finish Home Screen

--- ClientApp/src/components/home/Home.js
+++ ClientApp/src/components/home/Home.js
@@ -3,11 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/dashboard">
-                <p>Go to the Dashboard</p>
-            </Link>
+        <div className="bg-blue-600 h-screen w-screen flex flex-wrap">
+            <div className="mx-auto pt-36 w-full">
+                <div className="flex justify-around">
+                    <h1 className="text-5xl text-white font-bold pt-4 pb-2 cursor-default">Chelsea Project</h1>
+                </div>
+                <div className="flex justify-around">
+                    <p className="text-xs text-white font-light pb-4 cursor-default">Chelsea is a Project Management System that makes you productive.</p>
+                </div>
+                <Link to="/dashboard">
+                    <div className="flex justify-around">
+                        <button className="text-xl text-white py-2 px-4 border rounded-lg hover:bg-white hover:text-blue-600 active:shadow-inner">Go to the Dashboard</button>
+                    </div>
+                </Link>
+            </div>
+            <div className="mx-auto flex justify-around mb-12 mt-auto">
+                <p className="text-xs text-white font-light pt-32 cursor-default">Version: Pre-Alpha v1.0</p>
+            </div>
         </div>
     );
 }
